2cae1c7d912d5f7971d2881105b22969f363868e
krizhanovskiy <krizhanovskiy.a90@gmail.com>
2023-07-19 12:12:23 +0300

    image author info

--- frontend/src/components/ImageCard.js
+++ frontend/src/components/ImageCard.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card, NavLink } from 'react-bootstrap';
 
 const ImageCard = ({ image, deleteImage, saveImage }) => {
+  const authorName = image.user?.name || 'No author name';
+  const authorPortfolioURL = image.user?.portfolio_url;
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={image.urls.small} />
@@ -17,6 +19,14 @@ const ImageCard = ({ image, deleteImage, saveImage }) => {
           </Button>
         )}
       </Card.Body>
+      <Card.Footer className="text-center text-muted">
+        {authorPortfolioURL && (
+          <NavLink href={authorPortfolioURL} target="_blank">
+            {authorName}
+          </NavLink>
+        )}
+        {!authorPortfolioURL && authorName}
+      </Card.Footer>
     </Card>
   );
 };
